c0ac4378ab727dc91740a28f5c44310a233ccb00
Jacob Hsu <jacob.hsu.tw@gmail.com>
2015-06-11 21:44:20 +0800

    Using .keydown mario

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-    $('button').click(function() {
-    	var toAdd = $("input[name=message]").val();
-        $('#messages').append("<p>"+toAdd+"</p>");
-    });
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+			// Left arrow key pressed
+			case 37:
+				$('img').animate({left: "-=10px"}, 'fast');
+				break;
+			// Up Arrow Pressed
+			case 38:
+				$('img').animate({top: "-=10px"}, 'fast');
+				break;
+			// Right Arrow Pressed
+			case 39:
+				$('img').animate({left: "+=10px"}, 'fast');
+				break;
+			// Down Arrow Pressed
+			case 40:
+				$('img').animate({top: "+=10px"}, 'fast');
+				break;
+		}
+	});
 });
